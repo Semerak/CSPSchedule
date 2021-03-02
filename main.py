cf7f42879bba0a0cb1a543c1e5411bd658f7056e
Semerak <48312228+Semerak@users.noreply.github.com>
2021-03-02 04:54:10 +0200

    MRV

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from lib.csp import generate_schedule, convert_solution_to_schedule
 from data.data import *
 
 
-final_solution = generate_schedule(heuristic.Power, heuristic.first_value, time_slots, rooms, courses, groups, teachers)
+final_solution = generate_schedule(heuristic.MRV, heuristic.first_value, time_slots, rooms, courses, groups, teachers)
 if not final_solution:
     print("With this input, there is no solution")
 else:
